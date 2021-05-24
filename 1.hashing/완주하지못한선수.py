4ae340435cdd8f0247e90e0e3f67b5a6cd433ee1
SangilShim <84197419+SangilShim@users.noreply.github.com>
2021-05-24 17:13:06 +0900

    Update 완주하지못한선수.py

--- 1.hashing/완주하지못한선수.py
+++ 1.hashing/완주하지못한선수.py
@@ -14,3 +14,14 @@ def solution(participant, completion):
 입력값 〉	["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]
 기댓값 〉	"mislav"
 실행 결과 〉	실행한 결괏값 "stanko"이(가) 기댓값 "mislav"와(과) 다릅니다.
+
+# for 반복문이 제역학을 못해준다
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for a,b in zip(participant,completion):
+        if a != b:
+            return a
+        
+    return participant[-1]
