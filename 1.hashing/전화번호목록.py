51798dd55a5e0bcd6ff2d78607c03d882eed8591
SangilShim <84197419+SangilShim@users.noreply.github.com>
2021-05-24 17:12:23 +0900

    Update 전화번호목록.py

--- 1.hashing/전화번호목록.py
+++ 1.hashing/전화번호목록.py
@@ -15,3 +15,26 @@ def solution(phone_book):
 정확성: 66.7
 효율성: 0.0
 합계: 66.7 / 100.0
+
+# 구글링 풀이
+
+def solution(phone_book):
+    phone_book.sort()
+    for a,b in zip(phone_book,phone_book[1:]):
+        if b.startswith(a):
+            return False 
+
+    return True
+
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            answer = False
+            break
+    return answer
+
+# 해쉬를 이용한 풀이
+
